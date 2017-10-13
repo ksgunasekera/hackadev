a5d6225a7b8df3c975758b8b3434e67195274ff6
ksgunasekera <ksgunasekera@gmail.com>
2017-10-13 14:54:33 +0530

    Arduino Hackadev

---
+++ src/arduino/Arduino.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arduino;
+import com.fazecast.jSerialComm.*;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gunasekera
+ */
+public class Arduino {
+    private SerialPort ports[];
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       SerialPort ports[]=SerialPort.getCommPorts();
+       for(SerialPort port:ports){
+         System.out.println(port.getDescriptivePortName());
+        }
+       new GUI(ports).setVisible(true);
+       Scanner s=new Scanner(System.in);
+        //GUI g=new GUI();
+        //g.tomba();
+    }
+
+    /**
+     * @return the ports
+     */
+    public SerialPort[] getPorts() {
+        return ports;
+    }
+
+    /**
+     * @param ports the ports to set
+     */
+    public void setPorts(SerialPort[] ports) {
+        this.ports = ports;
+    }
+    
+}
